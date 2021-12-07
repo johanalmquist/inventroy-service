acdb0bb1e0a651529f412ec79477c9b33e061413
Johan Almquist <johan.almquist@aranya.se>
2021-12-07 13:24:17 +0100

    route to find site by id

--- src/routers/sites.py
+++ src/routers/sites.py
@@ -15,9 +15,22 @@ async def list_sites(db: Session = Depends(get_database)):
     return await site.get_all(db=db)
 
 
-@router.get("/site/{site_name}", response_model=schemas.Site)
-async def find_site(site_name, db: Session = Depends(get_database)):
-    return await site.find_by_name(site_name, db=db)
+@router.get("/site/{id}", response_model=schemas.Site)
+async def find_site(id: int, db: Session = Depends(get_database)):
+    db_site = await site.find(id=id, db=db)
+    if db_site is None:
+        raise HTTPException(status_code=404, detail="No site found!")
+
+    return db_site
+
+
+@router.get("/site/", response_model=schemas.Site)
+async def find_site_by_name(name: str, db: Session = Depends(get_database)):
+    db_site = await site.find_by_name(name=name, db=db)
+    if db_site is None:
+        raise HTTPException(status_code=404, detail="No site found!")
+
+    return db_site
 
 
 @router.post("/", response_model=schemas.Site)
